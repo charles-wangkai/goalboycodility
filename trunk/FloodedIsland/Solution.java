f7b5c7f354af5189d0ee2b0f7de269dd1cd8e7a3
charles.wangkai@gmail.com <charles.wangkai@gmail.com@679d2237-6a02-6d31-8ec3-7c748798977c>
2014-01-08 08:42:52 +0000

    FloodedIsland

--- trunk/FloodedIsland/Solution.java
+++ trunk/FloodedIsland/Solution.java
@@ -1,59 +1,39 @@
 package FloodedIsland;
 
 // Nitrogenium 2013
-// http://codility.com/cert/view/cert48QB3A-3JAX387ADYNNSPS6
+// http://codility.com/cert/view/certW7PM7W-XBDT7AFBEWVZ77K2
 
 import java.util.ArrayList;
 
 public class Solution {
 	@SuppressWarnings("unchecked")
 	public int[] solution(int[] A, int[] B) {
-		int maxA = getMax(A);
 		int maxB = getMax(B);
-		ArrayList<Integer>[] indices = new ArrayList[Math.max(maxA, maxB) + 1];
+		ArrayList<Integer>[] indices = new ArrayList[maxB + 1];
 		for (int i = 0; i < indices.length; i++) {
 			indices[i] = new ArrayList<Integer>();
 		}
 
 		for (int i = 0; i < A.length; i++) {
-			indices[A[i]].add(i);
+			if (A[i] <= maxB) {
+				indices[A[i]].add(i);
+			}
 		}
 
 		boolean[] underWaters = new boolean[A.length];
 		int[] islandNums = new int[maxB + 1];
 		int currentIslandNum = 1;
 		for (int i = 0; i < islandNums.length; i++) {
-			ArrayList<Integer> currentIndices = indices[i];
-			int rangeBegin = -1;
-			int rangeEnd = -1;
-			for (int j = 0; j <= currentIndices.size(); j++) {
-				int index = -1;
-				if (j < currentIndices.size()) {
-					index = currentIndices.get(j);
+			for (int index : indices[i]) {
+				int delta = -1;
+				if (index > 0 && !underWaters[index - 1]) {
+					delta++;
 				}
-				if (j < currentIndices.size() && rangeEnd >= 0
-						&& index == rangeEnd + 1) {
-					rangeEnd++;
-				} else {
-					if (rangeBegin >= 0) {
-						int delta = -1;
-						if (rangeBegin > 0 && !underWaters[rangeBegin - 1]) {
-							delta++;
-						}
-						if (rangeEnd < A.length - 1
-								&& !underWaters[rangeEnd + 1]) {
-							delta++;
-						}
-						currentIslandNum += delta;
-
-						for (int k = rangeBegin; k <= rangeEnd; k++) {
-							underWaters[k] = true;
-						}
-					}
-
-					rangeBegin = index;
-					rangeEnd = index;
+				if (index < A.length - 1 && !underWaters[index + 1]) {
+					delta++;
 				}
+				currentIslandNum += delta;
+				underWaters[index] = true;
 			}
 			islandNums[i] = currentIslandNum;
 		}
