8c581302edf7eebcb4c5f753ef41688819056652
charles.wangkai <charles.wangkai@gmail.com>
2014-01-12 08:17:42 +0000

    StoneWall

---
+++ StoneWall/Solution.java
@@ -0,0 +1,24 @@
+package StoneWall;
+
+// http://codility.com/demo/results/demoRX6A3A-2JY/
+
+import java.util.Stack;
+
+public class Solution {
+	public int solution(int[] H) {
+		int blockNum = 0;
+		Stack<Integer> heights = new Stack<Integer>();
+		for (int oneH : H) {
+			while (!heights.isEmpty() && heights.peek() > oneH) {
+				heights.pop();
+				blockNum++;
+			}
+			if (!heights.isEmpty() && heights.peek() == oneH) {
+				continue;
+			}
+			heights.push(oneH);
+		}
+		blockNum += heights.size();
+		return blockNum;
+	}
+}
