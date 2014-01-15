01efcdeedf2facf61c8572961965d3e287035210
charles.wangkai@gmail.com <charles.wangkai@gmail.com@679d2237-6a02-6d31-8ec3-7c748798977c>
2014-01-15 18:04:39 +0000

    Flags

---
+++ trunk/Flags/Solution.java
@@ -0,0 +1,29 @@
+package Flags;
+
+// http://codility.com/demo/results/demoPUZWTF-4MF/
+
+import java.util.ArrayList;
+
+public class Solution {
+	public int solution(int[] A) {
+		ArrayList<Integer> peaks = new ArrayList<Integer>();
+		for (int i = 1; i + 1 < A.length; i++) {
+			if (A[i] > A[i - 1] && A[i] > A[i + 1]) {
+				peaks.add(i);
+			}
+		}
+		for (int flagNum = 1;; flagNum++) {
+			int count = 0;
+			int lastPeak = -1;
+			for (int peak : peaks) {
+				if (lastPeak < 0 || peak - lastPeak >= flagNum) {
+					lastPeak = peak;
+					count++;
+				}
+			}
+			if (count < flagNum) {
+				return flagNum - 1;
+			}
+		}
+	}
+}
