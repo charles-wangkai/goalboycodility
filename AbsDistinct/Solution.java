65400b54302020a06307bdc3cd6feae4cf6132af
charles.wangkai <charles.wangkai@gmail.com>
2014-01-17 15:44:07 +0000

    AbsDistinct

--- AbsDistinct/Solution.java
+++ AbsDistinct/Solution.java
@@ -1,15 +1,31 @@
 package AbsDistinct;
 
-// http://codility.com/demo/results/demoPFAZPB-76F/
-
-import java.util.HashSet;
+// http://codility.com/demo/results/demoHXKNQX-B4A/
 
 public class Solution {
 	public int solution(int[] A) {
-		HashSet<Integer> absoluteValues = new HashSet<Integer>();
-		for (int elem : A) {
-			absoluteValues.add(Math.abs(elem));
+		int nonNegIndex = 0;
+		while (nonNegIndex < A.length && A[nonNegIndex] < 0) {
+			nonNegIndex++;
 		}
-		return absoluteValues.size();
+		int negIndex = nonNegIndex - 1;
+		int count = 0;
+		int prevAbsolute = -1;
+		while (negIndex >= 0 || nonNegIndex < A.length) {
+			int absolute;
+			if (nonNegIndex >= A.length
+					|| (negIndex >= 0 && -A[negIndex] < A[nonNegIndex])) {
+				absolute = -A[negIndex];
+				negIndex--;
+			} else {
+				absolute = A[nonNegIndex];
+				nonNegIndex++;
+			}
+			if (absolute != prevAbsolute) {
+				prevAbsolute = absolute;
+				count++;
+			}
+		}
+		return count;
 	}
 }
