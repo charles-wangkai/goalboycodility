f054ab869f19e910865d38679f069cc6ae9b9865
charles.wangkai <charles.wangkai@gmail.com>
2014-01-18 12:07:59 +0000

    FallingDisks

---
+++ FallingDisks/Solution.java
@@ -0,0 +1,27 @@
+package FallingDisks;
+
+// http://codility.com/demo/results/demoKQ3R5T-M29/
+
+import java.util.Stack;
+
+public class Solution {
+	public int solution(int[] A, int[] B) {
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int ring : A) {
+			stack.push(Math.min(ring, stack.isEmpty() ? Integer.MAX_VALUE
+					: stack.peek()));
+		}
+		int diskFitNum = 0;
+		for (int disk : B) {
+			while (!stack.isEmpty() && stack.peek() < disk) {
+				stack.pop();
+			}
+			if (stack.isEmpty()) {
+				break;
+			}
+			diskFitNum++;
+			stack.pop();
+		}
+		return diskFitNum;
+	}
+}
