016a8161c57abba4c35cabcf69be027f3518c5e0
charles.wangkai <charles.wangkai@gmail.com>
2014-01-18 12:20:06 +0000

    greasemonkey

--- greasemonkey/codility_solved_problem_marker.user.js
+++ greasemonkey/codility_solved_problem_marker.user.js
@@ -9,12 +9,19 @@
 // ==/UserScript==
 
 $(document).ready(function() {
+  var RENAMED_PROBLEMS = {
+    'FallingDiscs' : 'FallingDisks'
+  };
+
   var capitaliseFirstLetter = function(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
   
   var markSolvedProblem = function($problemName) {
     var name = $.map($problemName.html().trim().split('-'), capitaliseFirstLetter).join('');
+    if (name in RENAMED_PROBLEMS) {
+      name = RENAMED_PROBLEMS[name];
+    }
     var url = 'http://goalboycodility.googlecode.com/svn/trunk/' + name + '/';
     GM_xmlhttpRequest({
       method: 'GET',
