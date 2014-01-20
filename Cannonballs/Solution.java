fa7e1ec8600aba55a2fc7608c4d97da8f3d4f407
charles.wangkai <charles.wangkai@gmail.com>
2014-01-20 08:30:30 +0000

    Cannonballs

---
+++ Cannonballs/Solution.java
@@ -0,0 +1,40 @@
+package Cannonballs;
+
+// http://codility.com/demo/results/demoKQ2WXJ-XPG/
+
+import java.util.Arrays;
+
+public class Solution {
+	public int[] solution(int[] A, int[] B) {
+		int maxCannon = -1;
+		for (int cannon : B) {
+			maxCannon = Math.max(maxCannon, cannon);
+		}
+
+		int[] blockIndices = new int[maxCannon + 1];
+		Arrays.fill(blockIndices, Integer.MAX_VALUE);
+		int prevGround = -1;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] > prevGround) {
+				for (int j = prevGround + 1; j <= A[i]
+						&& j < blockIndices.length; j++) {
+					blockIndices[j] = i;
+				}
+				prevGround = A[i];
+			}
+		}
+
+		int[] A1 = Arrays.copyOf(A, A.length);
+		for (int cannon : B) {
+			int blockIndex = blockIndices[cannon];
+			if (blockIndex == 0 || blockIndex == Integer.MAX_VALUE) {
+				continue;
+			}
+			A1[blockIndex - 1]++;
+			blockIndices[A1[blockIndex - 1]] = Math.min(
+					blockIndices[A1[blockIndex - 1]], blockIndex - 1);
+		}
+
+		return A1;
+	}
+}
